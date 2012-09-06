3958c36e05fb4a501d6479b97eaa2be3e99e3efd
Piotr Majka <pmajka@nencki.gov.pl>
2012-09-06 11:57:34 +0200

    Average images class ready

--- multiprocessing_9_wreg.py
+++ multiprocessing_9_wreg.py
@@ -20,6 +20,7 @@ def getVectorString(iterable):
     return "x".join(map(str, map(int,iterable)))
 
 antsParam = namedtuple('antsParam', 'antsName defValue')
+genericParam= namedtuple('genericParam', 'name defValue argFormat')
 
 class antsWrapper(object):
     pass
@@ -57,8 +58,10 @@ class antsLinearRegistrationWrapper(antsWrapper):
              'affineIterations' : antsParam('number-of-affine-iterations', (10000,)*5),
              'affineGradientDescent' : antsParam('affine-gradient-descent-option', None)}
     
+    #XXX: Custom arguments parameter, custom serialization function
+    #XXX:
+    
     def __init__(self, outputNaming, metrics, dimension = 2, parameters = None):
-        
         self._assignDefaultValues()
         
         self.transformation = ('SyN', (0.15,))  # (str, (float, [float, ... ]))
@@ -83,11 +86,15 @@ class antsLinearRegistrationWrapper(antsWrapper):
                 setattr(self, parameter, data.defValue)
     
     def updateParameters(self, parameters):
-        for (name, value) in parameters:
+        for (name, value) in parameters.items():
             setattr(self, name, value)
+        return self
      
-    def generateWrapCommand(self):
-        pass
+    def generateWrap(self, metricIndex = 0, suffix = 'Deformed.nii.gz'):
+        movingImg = self.metrics[metricIndex].movingImage
+        referenceImg = self.metrics[metricIndex].fixedImage
+        outputImg = self.outputNaming + suffix
+        return antsReslice(movingImg, outputImg, referenceImage = referenceImg)
     
     def getParametricPrefix(self):
         pass
@@ -152,16 +159,104 @@ class antsLinearRegistrationWrapper(antsWrapper):
         output, errors = p.communicate()
         print  output, errors
      
+class antsReslice(antsWrapper):
+    _parameters = { \
+        'dimension'      : genericParam('dimension', 2, "%d"),
+        'movingImage'    : genericParam('movingImage', None, "%s"),
+        'outputImage'    : genericParam('outputImage', None, "%s"),
+        'referenceImage' : genericParam('referenceImage', None, "%s"),
+        'defomableWarpsList'  : genericParam('deformableList', [], "%s"),
+        'affineWarpList'  : genericParam('affineList', [], "%s"),
+        }
+    
+    _command = """WarpImageMultiTransform %(dimension)d \
+                  %(movingImage)s %(outputImage)s \
+                  -R %(referenceImage)s \
+                  %(deformableList)s %(affineList)s"""
+     
+    def __init__(self, movingImage, outputImage, referenceImage = None, \
+                 dimension = 2, parameters = None):
+        self._assignDefaultValues()
+        
+        self.dimension = dimension
+        self.movingImage = movingImage
+        self.outputImage = outputImage
+        
+        if referenceImage:
+            self.referenceImage = referenceImage
+        else:
+            self.referenceImage = self.movingImage
+        
+        if parameters != None:
+            self.updateParameters(parameters)
+    
+    def _assignDefaultValues(self):
+        for parameter, data in self._parameters.iteritems():
+            setattr(self, parameter, data.defValue)
+    
+    def updateParameters(self, parameters):
+        for (name, value) in parameters.items():
+            setattr(self, name, value)
+        return self
+    
+    def __call__(self):
+        command = str(self)
+        p = sub.Popen(command.split(), stdout=sub.PIPE, stderr=sub.PIPE)
+        output, errors = p.communicate()
+        print  output, errors
+        
+    def __str__(self):
+        commandDict = {}
+        commandDict['dimension'] = self.dimension
+        commandDict['movingImage'] = self.movingImage
+        commandDict['outputImage'] = self.outputImage
+        commandDict['referenceImage'] = self.referenceImage
+        commandDict['deformableList'] = " ".join(self.defomableWarpsList)
+        commandDict['affineList'] = " ".join(self.affineWarpList)
+        
+        return self._command % commandDict
 
-"""
-r = antsLinearRegistrationWrapper(outPrefix, metrics)
-r.
-
-"""
-
-
-class LinearRegressionTestCase(unittest.TestCase):
-
+class averageImages(object):
+    _command = """c%(dimension)dd  %(inputFiles)s -mean -type uchar -o %(outputFilename)s"""
+    
+    _parameters = { \
+            'input_files' : genericParam('inputFiles'   ,  [], '%s'), 
+            'output_file' : genericParam('outputFilename', [], '%s'),
+            'dimension' : genericParam('dimension', 2, '%d')
+            }
+    
+    def __init__(self, input_files, output_file):
+        self._assignDefaultValues()
+        
+        self.input_files = input_files
+        self.output_file = output_file
+    
+    def _assignDefaultValues(self):
+        for parameter, data in self._parameters.iteritems():
+            setattr(self, parameter, data.defValue)
+    
+    def updateParameters(self, parameters):
+        for (name, value) in parameters.items():
+            setattr(self, name, value)
+        return self
+    
+    def __call__(self):
+        command = str(self)
+        p = sub.Popen(command.split(), stdout=sub.PIPE, stderr=sub.PIPE)
+        output, errors = p.communicate()
+        print  output, errors
+    
+    def __str__(self):
+        commandDict = {}
+        commandDict['outputFilename'] = self.output_file
+        commandDict['inputFiles']     = " ".join(self.input_files)
+        commandDict['dimension']      = self.dimension
+        
+        return self._command % commandDict
+    
+class ConvertToVectorTestCase(unittest.TestCase):
+    """
+    """
     def test_check_valid_data(self):
         # Given
         vec = (10,-20,30)
@@ -205,6 +300,16 @@ class TestAntsMetric(unittest.TestCase):
         for ((args, kwargs), result) in zip(cases,desired):
             metric = str(antsIntensityMetric(*args, **kwargs))
             self.assertEqual(metric, result)
+
+
+class TestAntsWrapImage(unittest.TestCase):
+    
+    def test_simple_run(self):
+        wrapParams = {'defomableWarpsList' : 'x', 'affineWarp': 'x'}
+        result = \
+            antsReslice('moving.nii.gz','output.nii.gz').updateParameters(wrapParams)
+        desired = "WarpImageMultiTransform 2                   moving.nii.gz output.nii.gz                   -R moving.nii.gz                   x"
+        self.assertEqual(str(result).strip(), desired.strip())
     
 #   def test_simple_run(self):
 #       cases = [ \
@@ -217,8 +322,13 @@ class TestAntsMetric(unittest.TestCase):
 #       for ((args, kwargs), exception) in zip(cases, desired):
 #           self.assertRaises(exception, antsIntensityMetric, args, kwargs)
 
+a = antsLinearRegistrationWrapper("out", [antsIntensityMetric('f.nii.gz','m.nii.gz')])
+
         
 if __name__ == '__main__':
     #unittest.main()
     a = antsLinearRegistrationWrapper("out", [antsIntensityMetric('f.nii.gz','m.nii.gz')])
-    print a()
+    ww= a.generateWrap()
+    #print a()
+    #wrapParams = {'defomableWarpsList' : 'x', 'affineWarp': 'x'}
+    # antsReslice('moving.nii.gz','output.nii.gz').updateParameters(wrapParams)
