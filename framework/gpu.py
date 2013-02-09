e69c5448e73c69fb9a1d2d562e1c719aac998b94
Piotr Majka <pmajka@nencki.gov.pl>
2013-02-09 01:45:09 +0100

    Backup of single echannel volume renderer

--- framework/gpu.py
+++ framework/gpu.py
@@ -1,13 +1,14 @@
 # An example from scipy cookbook demonstrating the use of numpy arrys in vtk
 
 import vtk
+import os,sys
 import pos_palette
 
 imread = vtk.vtkStructuredPointsReader()
 imread.SetFileName('/home/pmajka/a.vtk')
 #imread.SetFileName('/home/pmajka/d.vtk')
 #imread.SetFileName('/home/pmajka/z.vtk')
-#imread.SetFileName('/dev/shm/a.vtk')
+#imread.SetFileName('/home/pmajka/x.vtk')
 imread.Update()
 
 cast = vtk.vtkImageCast()
@@ -17,45 +18,77 @@ cast.Update()
 
 extract = vtk.vtkExtractVOI()
 extract.SetInputConnection(cast.GetOutputPort())
-extract.SetVOI(00, 600, 00, 600, 0, 600)
+extract.SetVOI(00, 175, 0, 255, 0, 136)
 extract.SetSampleRate(1, 1, 1)
 
 class vtk_volume_mapper_wrapper():
-    def __init__(self):
-        pass
 
+    def __init__(self, image_data, use_multichannel_workflow=False):
+        self.acf = None
+        self.ctf = None
+        self.gof = None
 
-# The following class is used to store transparencyv-values for later retrival. In our case, we want the value 0 to be
-# completly opaque whereas the three different cubes are given different transperancy-values to show how it works.
-alphaChannelFunc = pos_palette.pos_palette.lib('grad_1').piecewise_function()
-colorFunc = pos_palette.pos_palette.lib('grayscale', min=0, max=255).color_transfer_function()
-gtfun = pos_palette.pos_palette.lib('grad_2').piecewise_function()
+        self.use_multichannel_workflow = use_multichannel_workflow
+        self.alpha_channel_function = None
+        self.color_transfer_function = None
+        self.gradient_opacity_function = None
 
-# The preavius two classes stored properties. Because we want to apply these properties to the volume we want to render,
-# we have to store them in a class that stores volume prpoperties.
-volumeProperty = vtk.vtkVolumeProperty()
-volumeProperty.SetColor(colorFunc)
-volumeProperty.SetScalarOpacity(alphaChannelFunc)
-volumeProperty.SetGradientOpacity(gtfun)
-volumeProperty.SetInterpolationTypeToLinear()
+        self.image_data = image_data
 
-# This class describes how the volume is rendered (through ray tracing).
-compositeFunction = vtk.vtkVolumeRayCastCompositeFunction()
-#compositeFunction = vtk.vtkVolumeRayCastIsosurfaceFunction()
-#compositeFunction.SetIsoValue(80)
+        self._min, self._max = (0, 255)
 
-# We can finally create our volume. We also have to specify the data for it, as well as how the data will be rendered.
-vp = vtk.vtkVolumeProperty()
-volumeMapper = vtk.vtkFixedPointVolumeRayCastMapper()
-volumeMapper.SetSampleDistance(0.05)
-volumeMapper.SetInputConnection(extract.GetOutputPort())
-#volumeMapper.SetBlendModeToComposite()
-#volumeProperty.IndependentComponentsOff()
+        self.volume_property = vtk.vtkVolumeProperty()
+        self.volume_mapper = vtk.vtkFixedPointVolumeRayCastMapper()
+        self.volume = vtk.vtkVolume()
 
-# The class vtkVolume is used to pair the preaviusly declared volume as well as the properties to be used when rendering that volume.
-volume = vtk.vtkVolume()
-volume.SetMapper(volumeMapper)
-volume.SetProperty(volumeProperty)
+    def _prepare_volume_property(self):
+        print self.alpha_channel_function.piecewise_function().GetRange()
+        self.volume_property.SetColor(self.color_transfer_function.color_transfer_function())
+        self.volume_property.SetScalarOpacity(self.alpha_channel_function.piecewise_function())
+        self.volume_property.SetGradientOpacity(self.gradient_opacity_function.piecewise_function())
+        self.volume_property.SetInterpolationTypeToLinear()
+
+        if self.use_multichannel_workflow == True:
+            self.volume_property.IndependentComponentsOff()
+
+    def _prepare_volume_mapper(self):
+        self.volume_mapper.SetSampleDistance(0.05)
+        self.volume_mapper.SetInputConnection(self.image_data.GetOutputPort())
+        self.volume_mapper.SetBlendModeToComposite()
+
+    def _prepare_volume(self):
+        self.volume.SetMapper(self.volume_mapper)
+        self.volume.SetProperty(self.volume_property)
+
+    def _load_transfer_functions(self):
+        if os.path.isfile(self.acf):
+            self.alpha_channel_function = \
+                    pos_palette.pos_palette.from_gnuplot_file(self.acf, min=self._min, max=self._max)
+        else:
+            self.alpha_channel_function = \
+                    pos_palette.pos_palette.lib(self.acf, min=self._min, max=self._max)
+
+        if os.path.isfile(self.ctf):
+            self.color_transfer_function = \
+                    pos_palette.pos_palette.from_gnuplot_file(self.ctf, min=self._min, max=self._max)
+        else:
+            self.color_transfer_function = \
+                    pos_palette.pos_palette.lib(self.ctf, min=self._min, max=self._max)
+
+        if os.path.isfile(self.gof):
+            self.gradient_opacity_function = \
+                    pos_palette.pos_palette.from_gnuplot_file(self.gof, min=self._min, max=self._max)
+        else:
+            self.gradient_opacity_function = \
+                    pos_palette.pos_palette.lib(self.gof, min=self._min, max=self._max)
+
+    def reload_configuration(self):
+        self._load_transfer_functions()
+        self._prepare_volume_property()
+        self._prepare_volume_mapper()
+        self._prepare_volume()
+
+        return self.volume
 
 # With almost everything else ready, its time to initialize the renderer and window, as well as creating a method for exiting the application
 renderer = vtk.vtkRenderer()
@@ -64,58 +97,32 @@ renderWin.AddRenderer(renderer)
 renderInteractor = vtk.vtkRenderWindowInteractor()
 renderInteractor.SetRenderWindow(renderWin)
 
+volume = vtk_volume_mapper_wrapper(extract)
+###############################################################################
+volume.acf = '/home/pmajka/02_02_NN2_mri_alpha_surface.gpf'
+volume.ctf = '/home/pmajka/02_02_NN2_mri_color_surface.gpf'
+volume.gof = '/home/pmajka/02_02_NN2_mri_gradient_surface.gpf'
+###############################################################################
+
+
+vol = volume.reload_configuration()
+
 # We add the volume to the renderer ...
-renderer.AddVolume(volume)
-# ... set background color to white ...
-renderer.SetBackground(1.0,1.0,1.0)
-# ... and set window size.
+renderer.AddVolume(vol)
+renderer.SetBackground(1.0, 1.0, 1.0)
 renderWin.SetSize(400, 400)
 
-# A simple function to be called when the user decides to quit the application.
-def exitCheck(obj, event):
-    if obj.GetEventPending() != 0:
-        obj.SetAbortRender(1)
+
+def Keypress(obj, event):
+    key = obj.GetKeySym()
+    if key.startswith('k'):
+        volume.reload_configuration()
+        renderWin.Render()
+
 
 # Tell the application to use the function as an exit check.
-renderWin.AddObserver("AbortCheckEvent", exitCheck)
-
-
-#   cube = vtk.vtkAnnotatedCubeActor()
-#   cube.GetXMinusFaceProperty().SetColor(1,0,0)
-#   cube.GetXPlusFaceProperty().SetColor(1,0,0)
-#   cube.GetYMinusFaceProperty().SetColor(0,1,0)
-#   cube.GetYPlusFaceProperty().SetColor(0,1,0)
-#   cube.GetZMinusFaceProperty().SetColor(0,0,1)
-#   cube.GetZPlusFaceProperty().SetColor(0,0,1)
-#   cube.GetTextEdgesProperty().SetColor(0,0,0)
-
-#   # anatomic labelling
-#   cube.SetXPlusFaceText ("R")
-#   cube.SetXMinusFaceText("L")
-#   cube.SetYPlusFaceText ("A")
-#   cube.SetYMinusFaceText("P")
-#   cube.SetZPlusFaceText ("S")
-#   cube.SetZMinusFaceText("I")
-
-#   axes = vtk.vtkAxesActor()
-#   axes.SetShaftTypeToCylinder()
-#   axes.SetTipTypeToCone()
-#   axes.SetXAxisLabelText("X")
-#   axes.SetYAxisLabelText("Y")
-#   axes.SetZAxisLabelText("Z")
-#   #axes.SetNormalizedLabelPosition(.5, .5, .5)
-
-#   orientation_widget = vtk.vtkOrientationMarkerWidget()
-#   orientation_widget.SetOrientationMarker(cube)
-#   orientation_widget.SetViewport(0.85,0.85,1.0,1.0)
-#   #orientation_widget.SetOrientationMarker(axes)
-#   orientation_widget.SetInteractor(renderInteractor)
-#   orientation_widget.SetEnabled(1)
-#   orientation_widget.On()
-#   orientation_widget.InteractiveOff()
-
+renderInteractor.AddObserver("KeyPressEvent", Keypress)
 
 renderInteractor.Initialize()
-# Because nothing will be rendered without any input, we order the first render manually before control is handed over to the main-loop.
 renderWin.Render()
 renderInteractor.Start()
