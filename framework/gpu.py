34dfb22ce4ae2f305df25380f5409d369fb8e072
Piotr Majka <pmajka@nencki.gov.pl>
2013-02-20 23:17:20 +0100

    ENCH: Skeleton of volume rendering wrapper is done. Trying to wrap vtkLightKit object

--- framework/gpu.py
+++ framework/gpu.py
@@ -6,7 +6,6 @@ from config import Config
 
 imread = vtk.vtkStructuredPointsReader()
 imread.SetFileName('/home/pmajka/mri.vtk')
-#imread.SetFileName('/home/pmajka/nietoperz_finished_uchar_small.vtk')
 imread.Update()
 
 cast0 = vtk.vtkImageCast()
@@ -33,57 +32,116 @@ extract1.SetInputConnection(cast1.GetOutputPort())
 extract1.SetVOI(0, 175*2, 0, 255*2, 00, 136*2)
 extract1.SetSampleRate(1, 1, 1)
 
+class vtk_volume_image_reader():
+    def __init__(self, filename, configuration_file):
+        self._configuration_filename = configuration_file
+
+    def _load_config_file(self):
+        self.cfg = Config(file(self._configuration_filename))
+        print self.cfg
+        for attr in ['acf', 'ctf', 'gof', 'use_multichannel_workflow','_min','_max']:
+            setattr(self, attr, self.cfg['mri_volume'][attr])
+
+    def _prepare_reader(self):
+        pass
+
+    def _prepare_image_cast(self):
+        pass
+
+    def _prepare_volume_subset(self):
+        pass
+
+    def _prepare_flip(self):
+        pass
+
+    def reload_configuration(self):
+        self._load_config_file()
+        self._load_transfer_functions()
+        self._prepare_volume_property()
+        self._prepare_volume_mapper()
+        self._prepare_volume()
+
+
+class vtk_light_kit():
+    def __init__(self, configuration_file):
+        self._configuration_filename = configuration_file
+        self.light_kit = vtk.vtkLightKit()
+
+        self.reload_configuration()
+
+    def _load_config_file(self):
+        self.cfg = Config(file(self._configuration_filename))
+        print self.cfg
+
+        attrmap = self.cfg['scene']['scene_lightning']
+        for attr, val in attrmap.iteritems():
+            getattr(self.light_kit, attr)(val)
+
+    def _prepare_light_kit(self):
+        self.light_kit.MaintainLuminanceOn()
+
+    def reload_configuration(self):
+        self._load_config_file()
+        self._prepare_light_kit()
+
+
 class vtk_volume_mapper_wrapper():
     """
     vtk_volume_mapper_wrapper(image_data,  use_multichannel_workflow=False)
-
     """
 
-    def __init__(self, image_data, use_multichannel_workflow=False):
+    def __init__(self, image_data, configuration_file):
         """
-        vtk_volume_mapper_wrapper(image_data,  use_multichannel_workflow=False)
+        vtk_volume_mapper_wrapper(image_data,configuration_file)
         """
-        self.acf = 'grad_ramp_one_to_zero'
-        self.ctf = 'grayscale'
-        self.gof = 'grad_constant_one'
 
-        self.configuration = None
+        self._configuration_filename = configuration_file
+        self._load_config_file()
+        self.image_data = image_data
 
-        self.use_multichannel_workflow = use_multichannel_workflow
         self.alpha_channel_function = None
         self.color_transfer_function = None
         self.gradient_opacity_function = None
-
-        self.image_data = image_data
-
-        self._min, self._max = (0, 255)
 
         self.volume_property = vtk.vtkVolumeProperty()
         self.volume_mapper = vtk.vtkFixedPointVolumeRayCastMapper()
         self.volume = vtk.vtkVolume()
 
+    def _load_config_file(self):
+        self.cfg = Config(file(self._configuration_filename))
+        print self.cfg
+        for attr in ['acf', 'ctf', 'gof', 'use_multichannel_workflow','_min','_max']:
+            setattr(self, attr, self.cfg['mri_volume'][attr])
+
     def _prepare_volume_property(self):
-        print self.alpha_channel_function.piecewise_function().GetRange()
         self.volume_property.SetColor(self.color_transfer_function.color_transfer_function())
         self.volume_property.SetScalarOpacity(self.alpha_channel_function.piecewise_function())
         self.volume_property.SetGradientOpacity(self.gradient_opacity_function.piecewise_function())
-        self.volume_property.SetInterpolationTypeToLinear()
-        #self.volume_property.ShadeOn()
+
+        # Just an alias:
+        attrmap = self.cfg['mri_volume']['volume_property']
+        for attr, val in attrmap.iteritems():
+            getattr(self.volume_property, attr)(val)
+            print  attr, val
+
         print self.volume_property.GetShade()
+        if self.volume_property.GetShade():
+            # Just an alias:
+            attrmap = self.cfg['mri_volume']['volume_property_shading']
+            for attr, val in attrmap.iteritems():
+                getattr(self.volume_property, attr)(val)
+                print attr, val
 
         if self.use_multichannel_workflow == True:
             self.volume_property.IndependentComponentsOff()
 
-        if self.volume_property.ShadeOn():
-            self.volume_property.SetDiffuse(0.5)
-            self.volume_property.SetSpecular(0.5)
-            self.volume_property.SetAmbient(0.5)
-            self.volume_property.SetSpecularPower(0.5)
-
     def _prepare_volume_mapper(self):
         self.volume_mapper.SetInputConnection(self.image_data.GetOutputPort())
-        self.volume_mapper.SetSampleDistance(0.01)
         self.volume_mapper.SetBlendModeToComposite()
+
+        attrmap = self.cfg['mri_volume']['volume_mapper']
+        for attr, val in attrmap.iteritems():
+            getattr(self.volume_mapper, attr)(val)
 
     def _prepare_volume(self):
         self.volume.SetMapper(self.volume_mapper)
@@ -95,6 +153,7 @@ class vtk_volume_mapper_wrapper():
         self.gradient_opacity_function = pos_palette.pos_palette.get(self.gof, min=self._min, max=self._max)
 
     def reload_configuration(self):
+        self._load_config_file()
         self._load_transfer_functions()
         self._prepare_volume_property()
         self._prepare_volume_mapper()
@@ -104,14 +163,9 @@ class vtk_volume_mapper_wrapper():
 
 # With almost everything else ready, its time to initialize the renderer and window, as well as creating a method for exiting the application
 renderer = vtk.vtkRenderer()
-renderer1 = vtk.vtkRenderer()
 
 renderWin = vtk.vtkRenderWindow()
 renderWin.AddRenderer(renderer)
-renderWin.AddRenderer(renderer1)
-
-renderer.SetViewport(0.0, 0.0, 0.5, 1.0)
-renderer1.SetViewport(0.5, 0.0, 1.0, 1.0)
 
 renderInteractor = vtk.vtkRenderWindowInteractor()
 renderInteractor.SetRenderWindow(renderWin)
@@ -120,11 +174,8 @@ renderer.GetCullers().InitTraversal()
 culler = renderer.GetCullers().GetNextItem()
 culler.SetSortingStyleToBackToFront()
 
-volume = vtk_volume_mapper_wrapper(extract)
+volume = vtk_volume_mapper_wrapper(extract, 'a.cfg')
 ###############################################################################
-volume.acf = 'grad_ramp_one_to_zero'
-volume.ctf = 'bb'
-volume.gof = 'grad_constant_one'
 #volume.acf = '/home/pmajka/bat_alpha.gpf'
 #volume.ctf = '/home/pmajka/bat_color.gpf'
 #volume.gof = '/home/pmajka/bat_gradient.gpf'
@@ -142,12 +193,12 @@ lightKit.AddLightsToRenderer(renderer)
 lightKit.MaintainLuminanceOn()
 lightKit.SetKeyLightIntensity(1.0)
 
-lightKit.SetKeyLightWarmth(0.65)
-lightKit.SetFillLightWarmth(0.6)
+lightKit.SetKeyLightWarmth(0.50)
+lightKit.SetFillLightWarmth(0.50)
 try :
-    lightKit.SetHeadLightWarmth(0.45)
+    lightKit.SetHeadLightWarmth(0.50)
 except :
-    lightKit.SetHeadlightWarmth(0.45)
+    lightKit.SetHeadlightWarmth(0.50)
 
 lightKit.SetKeyToFillRatio(2.)
 lightKit.SetKeyToHeadRatio(7.)
@@ -187,15 +238,10 @@ orientation_widget.SetEnabled(1)
 orientation_widget.On()
 orientation_widget.InteractiveOff()
 
-
-renderer1.SetActiveCamera(renderer.GetActiveCamera())
-
 # We add the volume to the renderer ...
-#renderer1.AddVolume(vol1)
 renderer.AddVolume(vol)
 renderer.SetBackground(1.0, 1.0, 1.0)
 renderWin.SetSize(400, 400)
-
 
 def Keypress(obj, event):
     key = obj.GetKeySym()
