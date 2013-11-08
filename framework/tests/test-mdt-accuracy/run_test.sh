49e4204bddaa37190793a69cb8318c2976c6b38f
Piotr Majka <pmajka@nencki.gov.pl>
2013-11-08 12:18:31 +0100

    Finishing the MDT calibration tests. No major outcome. Closing the branch. Bye.

--- framework/tests/test-mdt-accuracy/run_test.sh
+++ framework/tests/test-mdt-accuracy/run_test.sh
@@ -15,35 +15,44 @@ mu, sigma, n = $mu, $sigma, $n
 samples = np.random.normal(mu, sigma, size=n)
 
 for i in range(n):
-    v=samples[i]
-    print "convert -size ${image_size}x${image_size} xc:black -fill white -stroke none -draw \"rectangle %d,64 192,192\" img_%03d.png; c2d img_%03d.png -o %04d.nii.gz" %(v, i, i, i)
+    s=samples[i]
+    v=s
+    v2=192
+    print "convert -size ${image_size}x${image_size} xc:black -fill white -stroke none -draw \"rectangle %d,%d %d,%d\" img_%03d.png; c2d img_%03d.png -o %04d.nii.gz" %(v,v,v2,v2, i, i, i)
 END
 }
 
 generate_test_images | bash -x
-rm -vf *.png
 
-python ../../pos_mdt_main.py \
-    --jobId mdt_test_rectangle \
-    --workDir ${WORK_DIR} \
-    --antsDimension 2 \
-    --firstImageIndex 0 \
-    --lastImageIndex 99 \
-    --iterations 10 \
-    -i ./ \
-    --antsImageMetric CC \
-    --antsImageMetricOpt 5 \
-    --antsTransformation 0.25 \
-    --antsRegularization 3 0 \
-    --antsIterations 30x90x20 \
-    --antsAffineIterations 0 \
-    --loglevel DEBUG \
-    --settingsFile configuration.json 
+for vel_reg in 0 1 2 3 4 5
+do
+    for def_reg in 0 1 2 3 4 5
+    do
+        python ../../pos_mdt_main.py \
+            --jobId mdt_test_rectangle \
+            --workDir ${WORK_DIR} \
+            --antsDimension 2 \
+            --firstImageIndex 0 \
+            --lastImageIndex 99 \
+            --iterations 10 \
+            -i ./ \
+            --antsImageMetric CC \
+            --antsImageMetricOpt 5 \
+            --antsTransformation 0.25 \
+            --antsRegularization ${vel_reg} ${def_reg} \
+            --antsIterations  1000x1000x1000 \
+            --antsAffineIterations 0 \
+            --loglevel DEBUG  
+        #   --settingsFile configuration.json 
 
-cp -rfv ${WORK_DIR}/12_transf_f/sddm0009.nii.gz test_sddm.nii.gz
-cp -rfv ${WORK_DIR}/05_iterations/0009/21_resliced/average.nii.gz test_average.nii.gz
+        cp -rfv ${WORK_DIR}/12_transf_f/sddm_convergence.txt sddm_convergence_${vel_reg}_${def_reg}_sigma${sigma}.txt
+        cp -rfv ${WORK_DIR}/12_transf_f/sddm0009.nii.gz test_sddm_${vel_reg}_${def_reg}_sigma${sigma}.nii.gz
+        cp -rfv ${WORK_DIR}/05_iterations/0009/21_resliced/average.nii.gz test_average_${vel_reg}_${def_reg}_sigma${sigma}.nii.gz
 
-diff reference_sddm.nii.gz test_sddm.nii.gz > sddm_diff.txt
-diff reference_average.nii.gz test_average.nii.gz > average_diff.txt
+#       diff reference_sddm.nii.gz test_sddm.nii.gz > sddm_diff.txt
+#       diff reference_average.nii.gz test_average.nii.gz > average_diff.txt
+    done
+done
 
 rm -vf ????.nii.gz
+rm -vf img_???.png
