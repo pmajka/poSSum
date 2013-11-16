7df2f1accd9715f75209c4337e59e72c1b608475
Piotr Majka <pmajka@nencki.gov.pl>
2013-11-16 19:39:06 +0100

    ENCH: Further improvements to the sequential alignment script.

--- framework/pos_sequential_alignment.py
+++ framework/pos_sequential_alignment.py
@@ -5,14 +5,8 @@ import os, sys
 from optparse import OptionParser, OptionGroup
 import copy
 
-from pos_wrapper_skel
 import pos_wrappers
 import pos_parameters
-
-from pos_deformable_wrappers import preprocess_slice_volume,\
-                  visualize_wrap_field, convert_slice_image,\
-                  convert_slice_image_grayscale
-
 from pos_wrapper_skel import output_volume_workflow
 
 
@@ -22,7 +16,7 @@ class sequential_alignment(output_volume_workflow):
     """
 
     _f = {
-        'raw_image' : pos_parameters.filename('raw_image', work_dir = None, str_template = '{idx:04d}.nii.gz'),
+        'raw_image' : pos_parameters.filename('raw_image', work_dir = '00_override_this', str_template = '{idx:04d}.nii.gz'),
         'src_gray' : pos_parameters.filename('src_gray', work_dir = '00_source_gray', str_template='{idx:04d}.nii.gz'),
         'src_color' : pos_parameters.filename('src_color', work_dir = '01_source_color', str_template='{idx:04d}.nii.gz'),
         'part_transf' : pos_parameters.filename('part_transf', work_dir = '02_transforms', str_template='tr_m{mIdx:04d}_f{fIdx:04d}_Affine'),
@@ -39,33 +33,40 @@ class sequential_alignment(output_volume_workflow):
     def __init__(self, options, args):
         super(self.__class__, self).__init__(options, args)
 
+    def _initializeOptions(self):
+        super(self.__class__, self)._initializeOptions()
+
         #TODO: Provide input slice path and filename template a as comman line input
         # parameter
-
-        #TODO: Update the 'raw_image' workdir
-
         # Define the input slice range
         # XXX: Input slice range is assumed to be: <first_slice> <last_slice>
+        # TODO: Check validity of the input slices.
         self.options.slice_range = \
             range(self.options.sliceRange[0], self.options.sliceRange[1]+1)
 
-        # Override default raw_images directory (which, in fact, is just a
-        # stub) with the directory with actual images.
-        self.f['raw_images'].override_dir = self.options.inputVolume
+    def _overrideDefaults(self):
+        #TODO: Check if given directory exists! Validate of all the input file
+        # exists.
+        #TODO: This should be moved to the override_paths section of the script.
+        self.f['raw_image'].override_dir = self.options.inputImageDir
+        #TODO add is_file in pos_common module
 
     def launch(self):
         # Execute the parents before-execution activities
         super(self.__class__, self)._pre_launch()
 
+        # Prepare the input slices
+        self._generate_source_slices()
 
         # Run parent's post execution activities
         super(self.__class__, self)._post_launch()
 
     def _generate_source_slices(self):
 
+        commands = []
         for slice_number in self.options.slice_range:
-            pos_wrappers.alignment_preprocessor_wrapper(
-               intput_image = self.f['raw_image'](idx=slice_number),
+            command = pos_wrappers.alignment_preprocessor_wrapper(
+               input_image = self.f['raw_image'](idx=slice_number),
                grayscele_output_image = self.f['src_gray'](idx=slice_number),
                color_output_image = self.f['src_color'](idx=slice_number),
                registration_roi = self.options.registrationROI,
@@ -74,6 +75,9 @@ class sequential_alignment(output_volume_workflow):
                median_filter_radius = self.options.medianFilterRadius,
                invert_grayscale = self.options.invertGrayscale,
                invert_multichannel = self.options.invertMultichannel)
+            print command
+            commands.append(copy.deepcopy(command))
+        self.execute(commands)
 
     @classmethod
     def _getCommandLineParser(cls):
@@ -81,10 +85,10 @@ class sequential_alignment(output_volume_workflow):
 
         parser.add_option('--sliceRange', default=None,
             type='int', dest='sliceRange', nargs = 2,
-            help='Slice range. Requires two integers.')
+            help='Slice range. Requires two integers. REQUIRED')
         parser.add_option('--inputImageDir', default=None,
             type='str', dest='inputImageDir',
-            help='
+            help='')
 
         parser.add_option('--registrationROI', dest='registrationROI',
             default=None, type='int', nargs=4,
@@ -92,21 +96,23 @@ class sequential_alignment(output_volume_workflow):
         parser.add_option('--registrationResize', dest='registrationResize',
             default=None, type='float',
             help='Scaling factor for the source image used for registration. Float between 0 and 1.')
+        parser.add_option('--registrationColor',
+            dest='registrationColor', default='blue', type='str',
+            help='In rgb images - color channel on which \
+            registration will be performed. Has no meaning for \
+            grayscale input images. Possible values: r/red, g/green, b/blue.')
         parser.add_option('--medianFilterRadius', dest='medianFilterRadius',
             default=None, type='int', nargs=2,
             help='Median filter radius in voxels e.g. 2 2')
         parser.add_option('--invertGrayscale', dest='invertGrayscale',
-            default=False, action='store_const', const=True,
+            default=None, action='store_const', const=True,
             help='Invert source image: both, grayscale and multichannel, before registration')
         parser.add_option('--invertMultichannel', dest='invertMultichannel',
-            default=False, action='store_const', const=True,
+            default=None, action='store_const', const=True,
             help='Invert source image: both, grayscale and multichannel, before registration')
 
         # -----------------------------------------------------------
         return parser
-
-    def launch(self):
-        pass
 
 if __name__ == '__main__':
     options, args = sequential_alignment.parseArgs()
--- framework/pos_wrappers.py
+++ framework/pos_wrappers.py
@@ -751,7 +751,7 @@ class alignment_preprocessor_wrapper(generic_wrapper):
         'grayscele_output_image': filename_parameter('-g', None, str_template="{_name} {_value}"),
         'color_output_image': filename_parameter('-r', None, str_template="{_name} {_value}"),
         'registration_roi': list_parameter('registrationROI', None, str_template="--{_name} {_list}"),
-        'registration_resize': list_parameter('registrationResize', None, str_template="--{_name} {_list}"),
+        'registration_resize': value_parameter('registrationResize', None, str_template="--{_name} {_value}"),
         'registration_color': string_parameter('registrationColorChannel', None, str_template="--{_name} {_value}"),
         'median_filter_radius': list_parameter('medianFilterRadius', None, str_template="--{_name} {_list}"),
         'invert_grayscale': switch_parameter('invertSourceImage', False, str_template="--{_name}"),
