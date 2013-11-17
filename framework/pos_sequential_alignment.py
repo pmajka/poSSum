16c8129e69131dabbf0bd9f4928ac5257a9ba816
Piotr Majka <pmajka@nencki.gov.pl>
2013-11-17 11:14:17 +0100

    ENCH: Validation if input file exist in sequential alignment workflow.

--- framework/pos_sequential_alignment.py
+++ framework/pos_sequential_alignment.py
@@ -19,8 +19,9 @@ class sequential_alignment(output_volume_workflow):
         'raw_image' : pos_parameters.filename('raw_image', work_dir = '00_override_this', str_template = '{idx:04d}.nii.gz'),
         'src_gray' : pos_parameters.filename('src_gray', work_dir = '00_source_gray', str_template='{idx:04d}.nii.gz'),
         'src_color' : pos_parameters.filename('src_color', work_dir = '01_source_color', str_template='{idx:04d}.nii.gz'),
-        'part_transf' : pos_parameters.filename('part_transf', work_dir = '02_transforms', str_template='tr_m{mIdx:04d}_f{fIdx:04d}_Affine'),
-        'comp_transf' : pos_parameters.filename('comp_transf', work_dir = '02_transforms', str_template='ct_m{mIdx:04d}_f{fIdx:04d}_Affine'),
+        'part_naming' : pos_parameters.filename('part_naming', work_dir = '02_transforms', str_template='tr_m{mIdx:04d}_f{fIdx:04d}_Affine'),
+        'part_transf' : pos_parameters.filename('part_transf', work_dir = '02_transforms', str_template='tr_m{mIdx:04d}_f{fIdx:04d}_Affine.txt'),
+        'comp_transf' : pos_parameters.filename('comp_transf', work_dir = '02_transforms', str_template='ct_m{mIdx:04d}_f{fIdx:04d}_Affine.txt'),
         'resliced_gray' : pos_parameters.filename('resliced_gray', work_dir = '04_gray_resliced', str_template='{idx:04d}.nii.gz'),
         'resliced_color' : pos_parameters.filename('resliced_color', work_dir = '05_color_resliced', str_template='{idx:04d}.nii.gz'),
         'out_volume_gray' : pos_parameters.filename('out_volume_gray', work_dir = '06_output_volumes', str_template='{fname}_gray.nii.gz'),
@@ -36,9 +37,10 @@ class sequential_alignment(output_volume_workflow):
     def _initializeOptions(self):
         super(self.__class__, self)._initializeOptions()
 
+        # Define slices' range. All slices within given range will be
+        # processed.
         self.options.slice_range = \
-            range(self.options.sliceRange[0], self.options.sliceRange[1]+1)
-
+            range(self.options.sliceRange[0], self.options.sliceRange[1] + 1)
 
     def _overrideDefaults(self):
         self.f['raw_image'].override_dir = self.options.inputImageDir
@@ -46,6 +48,18 @@ class sequential_alignment(output_volume_workflow):
     def launch(self):
         # Execute the parents before-execution activities
         super(self.__class__, self)._pre_launch()
+
+        # Verify if all the files are availabe:
+        # All images have to be ailable. If the case is different, the workflow
+        # will not proceed.
+        for slice_index in self.options.slice_range:
+            slice_filename = self.f['raw_image'](idx=slice_index)
+
+            self._logger.debug("Checking for image: %s.", slice_filename)
+            if not os.path.isfile(slice_filename):
+                self._logger.error("File does not exist: %s. Exiting",
+                                   slice_filename)
+                sys.exit(1)
 
         # Before launching the registration process check, if the intput
         # directory and all the input images exist.
@@ -88,51 +102,54 @@ class sequential_alignment(output_volume_workflow):
     def _calculate_transforms(self):
         """
         """
-        # The two loops below have to stay separated
+        # Calculate partial transforms - get partial transformation chain;
+        partial_transformation_pairs = \
+            map(lambda idx: self._get_slice_pair(idx),
+                            self.options.slice_range)
 
-        # Calculate partial transforms;
-        for moving_slice_index in self.options.slice_range:
-            self._calculate_individual_partial_transform(moving_slice_index)
+        commands = map(lambda x: self._get_partial_transform(*x),
+                                 partial_transformation_pairs)
+        self.execute(commands)
 
         # Calculate composite transforms
+        commands = []
         for moving_slice_index in self.options.slice_range:
-            self._calculate_individual_composed_transform(moving_slice_index)
+            commands.append(self._calculate_individual_composed_transform(moving_slice_index))
+        self.execute(commands)
 
-    def _get_partial_transform(self, moving_slice_index):
+    def _get_slice_pair(self, moving_slice_index):
         i = moving_slice_index
         s, e, r = tuple(self.options.sliceRange)
 
-        ret_dict= []
-        if i==r:
-            j=i
-            ret_dict.append((i, j))
+        if i == r:
+            j = i
+            retval = (i, j)
         if i > r:
-            j = i-1
-            ret_dict.append((i, j))
+            j = i - 1
+            retval = (i, j)
         if i < r:
-            j = i+1
-            ret_dict.append((i, j))
-        return ret_dict
+            j = i + 1
+            retval = (i, j)
+        return retval
 
-    def _calculate_individual_partial_transform(self, moving_slice_index):
-        #XXX TODO: Here's the place where you stopped!
-        transforms_chain = self._get_partial_transform(moving_slice_index)
-        moving_slice_index, fixed_slice_index = transforms_chain[0]
-
+    def _get_partial_transform(self, moving_slice_index, fixed_slice_index):
+        """
+        """
         similarity_metric = self.options.antsImageMetric
         metric_parameter = self.options.antsImageMetricOpt
-        affine_iterations = [10000,10000,10000,10000,10000]
-        output_naming = self.f['part_transf'](mIdx=moving_slice_index, fIdx=fixed_slice_index)
+        affine_iterations = [10000, 10000, 10000, 10000, 10000]
+        output_naming = self.f['part_naming'](mIdx=moving_slice_index,
+                                              fIdx=fixed_slice_index)
         use_rigid_transformation = self.options.useRigidAffine
         affine_metric_type = self.options.antsImageMetric
 
-        metrics  = []
+        metrics = []
         metric = pos_wrappers.ants_intensity_meric(
-                    fixed_image  = self.f['src_gray'](idx=fixed_slice_index),
-                    moving_image = self.f['src_gray'](idx=moving_slice_index),
-                    metric = similarity_metric,
-                    weight = 1.0,
-                    parameter = metric_parameter)
+            fixed_image=self.f['src_gray'](idx=fixed_slice_index),
+            moving_image=self.f['src_gray'](idx=moving_slice_index),
+            metric=similarity_metric,
+            weight=1.0,
+            parameter=metric_parameter)
         metrics.append(copy.deepcopy(metric))
 
         registration = pos_wrappers.ants_registration(
@@ -147,11 +164,65 @@ class sequential_alignment(output_volume_workflow):
             miOption = [metric_parameter, 16000],
             affineMetricType = affine_metric_type)
 
-        print registration
-        #commands.append(copy.deepcopy(registration))
+        return copy.deepcopy(registration)
+
+    def _get_transformation_chain(self, moving_slice_index):
+        i = moving_slice_index
+        s, e, r = tuple(self.options.sliceRange)
+
+        retDict = []
+
+        if i == r:
+            retDict.append((i, i))
+        if i > r:
+            for j in list(reversed(range(r, i))):
+                retDict.append((j + 1, j))
+        if i < r:
+            for j in range(i, r):
+                retDict.append((j, j + 1))
+        return retDict
 
     def _calculate_individual_composed_transform(self, moving_slice_index):
-        pass
+        """
+        """
+
+        transformation_chain = \
+            self._get_transformation_chain(moving_slice_index)
+
+        # Initialize the partial transforms array and then collect all partial
+        # transformations constituting given composite transformation.
+        partial_transformations = []
+        for (m_slice, r_slice) in transformation_chain:
+            partial_transformations.append(
+                self.f['part_transf'](mIdx=m_slice, fIdx=r_slice))
+
+        composite_transform_filename = \
+            self.f['comp_transf'](mIdx=m_slice, fIdx=r_slice)
+
+        command = pos_wrappers.ants_compose_multi_transform(
+            dimension = 2,
+            output_image = composite_transform_filename,
+            reference_image = self.f['src_gray'](idx=r_slice),
+            deformable_list = [],
+            affine_list = partial_transformations)
+
+        return copy.deepcopy(command)
+
+    def _reslice(self):
+        for slice_index in self.options.slice_range:
+            self._resliceGrayscale(slice_index)
+            self._resliceColor(slice_index)
+
+    def _resliceGrayscale(self, sliceNumber):
+        moving_image = self.f['src_gray'](idx=sliceNumber)
+        resliced_image = self.f['resliced_gray'](idx=sliceNumber)
+        reference_image = self.f['src_gray'](idx=self.options.slice_range[2])
+        transformation_file = self.f['comp_transf'](mIdx=sliceNumber,fIdx=self.options.slice_range[2])
+
+        #cmdDict['region'] = self._getOutputVolumeROIstr(strType = 'c2d')
+        #command = COMMAND_WARP_GRAYSCALE_SLICE % cmdDict
+        #executeSystem(command)
+
 
     @classmethod
     def _getCommandLineParser(cls):
