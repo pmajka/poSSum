285861d74ae69e34a36283c9242c05c1cc8a9063
Piotr Majka <pmajka@nencki.gov.pl>
2013-12-27 22:10:15 +0100

    ENCH: Preprocessor works better and better. Time to make it objective.

--- framework/pos_input_data_preprocessor.py
+++ framework/pos_input_data_preprocessor.py
@@ -12,9 +12,17 @@ import math
 
 from PIL import Image
 
-SOURCE_IMAGES_DIR='/home/pmajka/possum/data/03_02_NN4/66_histology_extracted_slides/'
+# TODO: Validate column names
+# TODO: Validate image filenames
+# TODO: Validate masks filenames.
+
+#SOURCE_IMAGES_DIR='/home/pmajka/possum/data/03_02_NN4/66_histology_extracted_slides/'
+SOURCE_IMAGES_DIR='/home/pmajka/possum/data/03_01_NN3/66_histology_extracted_slides/'
 _ROW_OF_THE_FIRST_SLICE = 14 - 1
 _DEFAULT_PADDING_ROUNDING = 100
+_DEFAULT_SHEET_INDEX = 0
+
+_CELL_STACK_SIZE = (3, 2)
 
 _SLICE_INDEX_COLUMN = 1 # column b
 _IMAGE_NAME_COLUMN = 2 # column c
@@ -35,6 +43,40 @@ _FILE_SIZE_COLUMN = 17 # column R
 _FILE_HASH_COLUMN = 18 # column S
 _PADDING_COLUMN = 19 # column T
 
+COLUMN_MAPPING = {
+    'image_index' : (_SLICE_INDEX_COLUMN, int),
+    'image_name' : (_IMAGE_NAME_COLUMN, str),
+    'mask_name' : (_IMAGE_MASK_COLLUMN, str),
+    'image_resolution' : (_IMAGE_RESOLUTION_COLUMN, float),
+    'reference_image_index' : (_REFERENCE_PLATE_INDEX_COLUMN, int),
+    'reference_coordinate' : (_REFERENCE_COORDINATE_COLUMN, float),
+    'distortion' : (_DISTORTION_INDEX_COLUMN, str),
+    'replacement_index' : (_REPLACEMENT_INDEX_COLUMN, int),
+    'process_resolution' : (_PROCESSING_RESOLUTION, float),
+    'rotation' : (_ROTATION_COLUMN, int),
+    'horizontal_flip' : (_HORIZONTAL_FLIP_COLLUMN, bool),
+    'vertical_flip' : (_VERTICAL_FLIP_COLUMN, bool),
+    'image_size' : (_IMAGE_SIZE_COLUMN, list),
+    'file_size' : (_FILE_SIZE_COLUMN, int),
+    'image_hash' : (_FILE_HASH_COLUMN, str),
+    'padded_size' : (_PADDING_COLUMN, list)}
+
+def read_image_metadata_from_sheet(attribute, slice_index):
+    column, converter = COLUMN_MAPPING[attribute]
+    row = get_row_index(slice_index)
+    try:
+        value = converter(s.cell_value(row, column))
+    except:
+        value = None
+    return value
+
+def write_image_metadata(attribute, slice_index, value):
+    column, converter = COLUMN_MAPPING[attribute]
+    row = get_row_index(slice_index)
+    if attribute in ['image_size', 'padded_size']:
+        value = "x".join(map(str, value))
+    wb.get_sheet(0).write(row, column, str(value))
+
 def round_custom(value, level = _DEFAULT_PADDING_ROUNDING):
     return math.ceil(value / level + 1) * level
 
@@ -49,23 +91,15 @@ def md5sum(filename):
             md5.update(chunk)
     return md5.hexdigest()
 
-
 def get_image_size_pixels(filename):
     im = Image.open(filename)
     width, height = im.size
     return width, height
 
-
-def get_padding(images):
-    max_w = max(map(lambda x: x.image_size[0], images.values()))
-    max_h = max(map(lambda x: x.image_size[1], images.values()))
-    pad_w = round_custom(max_w)
-    pad_h = round_custom(max_h)
-    return pad_w, pad_h
-
 class input_image(object):
 
     def __init__(self):
+        self.index = None
         self.image_index = None
         self.image_name = None
         self.mask_name = None
@@ -91,16 +125,36 @@ class input_image(object):
     def __str__(self):
         return str(self.__dict__)
 
+def get_row_index(slice_index):
+    return _ROW_OF_THE_FIRST_SLICE + slice_index
+
+def determine_and_set_file_size(new_slice):
+    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
+    stat = os.stat(filename)
+    return stat.st_size
+
+def determine_and_set_image_hash(new_slice):
+    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
+    return md5sum(filename)
+
+def determine_and_set_image_size(new_slice):
+    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
+    return get_image_size_pixels(filename)
+
+def determine_and_set_padding(new_slice = None):
+    max_w = max(map(lambda x: x.image_size[0], images.values()))
+    max_h = max(map(lambda x: x.image_size[1], images.values()))
+    pad_w = round_custom(max_w)
+    pad_h = round_custom(max_h)
+    return pad_w, pad_h
+
 
 b = xlrd.open_workbook('/home/pmajka/Dropbox/administracja/u.xls', formatting_info=True)
-s = b.sheet_by_index(0)
+s = b.sheet_by_index(_DEFAULT_SHEET_INDEX)
 wb = xlutils.copy.copy(b)
-stack_size = int(s.cell(3,2).value)
+stack_size = int(s.cell(*_CELL_STACK_SIZE).value)
 print "Stack size", stack_size
 
-# TODO: Validate column names
-# TODO: Validate image filenames
-# TODO: Validate masks filenames.
 
 # Create a dictionary for all the slices within given file
 images = {}
@@ -108,64 +162,31 @@ images = {}
 
 # -------------------------------------------------------------------
 # Extract all data related with a given slice.
+metadata_to_load = ['image_index', 'image_name', 'mask_name', 'image_resolution',
+                    'reference_image_index', 'reference_coordinate', 'replacement_index',
+                    'distortion', 'process_resolution', 'rotation', 'horizontal_flip',
+                    'vertical_flip']
+metadata_to_establish = ['file_size', 'image_hash', 'image_size']
 
 for slice_index in range(stack_size):
     new_slice = input_image()
-    new_slice.image_index = int(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _SLICE_INDEX_COLUMN).value)
-    new_slice.image_name = str(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _IMAGE_NAME_COLUMN).value)
-    new_slice.image_mask = str(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _IMAGE_MASK_COLLUMN).value)
-    new_slice.image_resolution = float(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _IMAGE_RESOLUTION_COLUMN).value)
+    for metadata in metadata_to_load:
+        setattr(new_slice, metadata, read_image_metadata_from_sheet(metadata, slice_index))
 
-    cell_value = s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _REFERENCE_PLATE_INDEX_COLUMN).value
-    try:
-        value = int(cell_value)
-    except:
-        value = None
-    new_slice.reference_image_index = value
-
-    cell_value = s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _REFERENCE_COORDINATE_COLUMN).value
-    try:
-        value = float(cell_value)
-    except:
-        value = None
-    new_slice.reference_coordinate = value
-
-    new_slice.image_mask = str(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _DISTORTION_INDEX_COLUMN).value)
-
-    cell_value = s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _REPLACEMENT_INDEX_COLUMN).value
-    try:
-        value = int(cell_value)
-    except:
-        value = None
-    new_slice.replacement_index = value
-    wb.get_sheet(0).write(_ROW_OF_THE_FIRST_SLICE + slice_index, _REPLACEMENT_INDEX_COLUMN, str(new_slice.replacement_index).lower())
-
-    new_slice.process_resolution = float(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _PROCESSING_RESOLUTION).value)
-    new_slice.rotation = int(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _ROTATION_COLUMN).value)
-    new_slice.horizontal_flip = bool(int(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _HORIZONTAL_FLIP_COLLUMN).value))
-    new_slice.vertical_flip = bool(int(s.cell(_ROW_OF_THE_FIRST_SLICE + slice_index, _VERTICAL_FLIP_COLUMN).value))
-
-    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
-    stat = os.stat(filename)
-    new_slice.file_size = stat.st_size
-    wb.get_sheet(0).write(_ROW_OF_THE_FIRST_SLICE + slice_index, _FILE_SIZE_COLUMN, new_slice.file_size)
-
-    new_slice.image_hash = md5sum(filename)
-    wb.get_sheet(0).write(_ROW_OF_THE_FIRST_SLICE + slice_index, _FILE_HASH_COLUMN, new_slice.image_hash)
-
-    new_slice.image_size = get_image_size_pixels(filename)
-    wb.get_sheet(0).write(_ROW_OF_THE_FIRST_SLICE + slice_index, _IMAGE_SIZE_COLUMN, "x".join(map(str,new_slice.image_size)))
-
-    new_slice.padded_size = None
+    for metadata in metadata_to_establish:
+        value = globals()['determine_and_set_' + metadata](new_slice)
+        setattr(new_slice, metadata, value)
+        write_image_metadata(metadata, slice_index, getattr(new_slice,metadata))
 
     images[new_slice.image_index] = new_slice
 
     print new_slice
 
-padded_image_size = tuple(map(int,get_padding(images)))
+padded_image_size = tuple(map(int, determine_and_set_padding()))
 map(lambda x: setattr(x,'padded_size', padded_image_size), images.values())
-# Put padding into the excel file:
+
 for slice_index in range(stack_size):
-    wb.get_sheet(0).write(_ROW_OF_THE_FIRST_SLICE + slice_index, _PADDING_COLUMN, "x".join(map(str,new_slice.padded_size)))
+    write_image_metadata('padded_size', slice_index, padded_image_size)
+
 
 wb.save(os.path.join('/home/pmajka','output.xls'))
