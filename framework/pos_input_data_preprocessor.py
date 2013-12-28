1d0e94cb9abd73071b97c5436621dd93a9db67ee
Piotr Majka <pmajka@nencki.gov.pl>
2013-12-28 18:01:06 +0100

    ENCH: Preprocessor works better and better. Time to make it objective.

--- framework/pos_input_data_preprocessor.py
+++ framework/pos_input_data_preprocessor.py
@@ -24,12 +24,12 @@ _DEFAULT_SHEET_INDEX = 0
 
 _CELL_STACK_SIZE = (3, 2)
 
-_SLICE_INDEX_COLUMN = 1 # column b
-_IMAGE_NAME_COLUMN = 2 # column c
-_IMAGE_MASK_COLLUMN = 3 # column d
-_IMAGE_RESOLUTION_COLUMN = 4 # column e
-_REFERENCE_PLATE_INDEX_COLUMN = 5 # column f
-_REFERENCE_COORDINATE_COLUMN = 6 # column g
+_SLICE_INDEX_COLUMN = 1 # column B
+_IMAGE_NAME_COLUMN = 2 # column C
+_IMAGE_MASK_COLLUMN = 3 # column D
+_IMAGE_RESOLUTION_COLUMN = 4 # column E
+_REFERENCE_PLATE_INDEX_COLUMN = 5 # column F
+_REFERENCE_COORDINATE_COLUMN = 6 # column G
 _DISTORTION_INDEX_COLUMN = 8 # column I
 _REPLACEMENT_INDEX_COLUMN = 9 # column J
 
@@ -76,22 +76,6 @@ def md5sum(filename):
             md5.update(chunk)
     return md5.hexdigest()
 
-def read_image_metadata_from_sheet(attribute, slice_index):
-    column, converter = COLUMN_MAPPING[attribute]
-    row = get_row_index(slice_index)
-    try:
-        value = converter(worksheet.cell_value(row, column))
-    except:
-        value = None
-    return value
-
-def write_image_metadata(attribute, slice_index, value):
-    column, converter = COLUMN_MAPPING[attribute]
-    row = get_row_index(slice_index)
-    if attribute in ['image_size', 'padded_size']:
-        value = "x".join(map(str, value))
-    workbook_writer.get_sheet(0).write(row, column, str(value))
-
 def get_image_size_pixels(filename):
     im = Image.open(filename)
     width, height = im.size
@@ -126,73 +110,102 @@ class input_image(object):
     def __str__(self):
         return str(self.__dict__)
 
-def get_row_index(slice_index):
-    return _ROW_OF_THE_FIRST_SLICE + slice_index
 
-def determine_and_set_file_size(new_slice):
-    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
-    stat = os.stat(filename)
-    return stat.st_size
 
-def determine_and_set_image_hash(new_slice):
-    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
-    return md5sum(filename)
-
-def determine_and_set_image_size(new_slice):
-    filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
-    return get_image_size_pixels(filename)
-
-def determine_and_set_padding(new_slice = None):
-    max_w = max(map(lambda x: x.image_size[0], images.values()))
-    max_h = max(map(lambda x: x.image_size[1], images.values()))
-    pad_w = round_custom(max_w)
-    pad_h = round_custom(max_h)
-    return pad_w, pad_h
-
+workbook_in = '/home/pmajka/Dropbox/administracja/u.xls'
 
 class worksheet_manager(object):
+    self._metadata_to_load = ['image_index', 'image_name', 'mask_name',
+        'image_resolution', 'reference_image_index', 'reference_coordinate',
+        'replacement_index', 'distortion', 'process_resolution', 'rotation',
+        'horizontal_flip', 'vertical_flip']
+    self._metadata_to_establish = ['file_size', 'image_hash', 'image_size']
+
     def __init__(self, workbook_in, workbook_out):
-        pass
 
+        self._workbook_in = workbook_in
+        self._workbook_out = workbook_out
 
+        self._workbook = xlrd.open_workbook(workbook_in, formatting_info=True)
+        self._worksheet = self._workbook.sheet_by_index(_DEFAULT_SHEET_INDEX)
+        self._workbook_writer = xlutils.copy.copy(self._workbook)
 
-workbook = xlrd.open_workbook('/home/pmajka/Dropbox/administracja/u.xls', formatting_info=True)
-worksheet = workbook.sheet_by_index(_DEFAULT_SHEET_INDEX)
-workbook_writer = xlutils.copy.copy(workbook)
-stack_size = int(worksheet.cell(*_CELL_STACK_SIZE).value)
-print "Stack size", stack_size
+        self._stack_size = int(self._worksheet.cell(*_CELL_STACK_SIZE).value)
+        print "Stack size", self._stack_size
 
+        # Create a dictionary for all the slices within given file
+        self._images = {}
 
-# Create a dictionary for all the slices within given file
-images = {}
+    def process(self):
+        self.load_metadata_from_workbook()
+        self.calculate_padding()
+        self.save_workbook()
 
-# -------------------------------------------------------------------
-# Extract all data related with a given slice.
-metadata_to_load = ['image_index', 'image_name', 'mask_name', 'image_resolution',
-                    'reference_image_index', 'reference_coordinate', 'replacement_index',
-                    'distortion', 'process_resolution', 'rotation', 'horizontal_flip',
-                    'vertical_flip']
-metadata_to_establish = ['file_size', 'image_hash', 'image_size']
+    def load_metadata_from_workbook(self):
+        for slice_index in range(self._stack_size):
+            new_slice = input_image()
 
-for slice_index in range(stack_size):
-    new_slice = input_image()
-    for metadata in metadata_to_load:
-        setattr(new_slice, metadata, read_image_metadata_from_sheet(metadata, slice_index))
+            for metadata in self._metadata_to_load:
+                value = self._read_from_workbook(metadata, slice_index)
+                setattr(new_slice, metadata, value)
 
-    for metadata in metadata_to_establish:
-        value = globals()['determine_and_set_' + metadata](new_slice)
-        setattr(new_slice, metadata, value)
-        write_image_metadata(metadata, slice_index, getattr(new_slice,metadata))
+            for metadata in self._metadata_to_establish:
+                value = getattr(self, ['determine_and_set_' + metadata])(new_slice)
+                setattr(new_slice, metadata, value)
+                self._write_to_workbook(metadata, slice_index, getattr(new_slice, metadata))
 
-    images[new_slice.image_index] = new_slice
+            self._images[new_slice.image_index] = new_slice
 
-    print new_slice
+    def save_workbook(self):
+        workbook_writer.save(self._workbook_out)
 
-padded_image_size = tuple(map(int, determine_and_set_padding()))
-map(lambda x: setattr(x,'padded_size', padded_image_size), images.values())
+    def calculate_padding(self):
+        padded_image_size = tuple(map(int, self.determine_and_set_padding()))
+        map(lambda x: setattr(x,'padded_size', padded_image_size), self.images.values())
 
-for slice_index in range(stack_size):
-    write_image_metadata('padded_size', slice_index, padded_image_size)
+        for slice_index in range(self._stack_size):
+            self._write_to_workbook('padded_size', slice_index, padded_image_size)
 
+    def _read_from_workbook(self, attribute, slice_index):
+        column, converter = COLUMN_MAPPING[attribute]
+        row = self._get_row_index(slice_index)
+        try:
+            value = converter(self._worksheet.cell_value(row, column))
+        except:
+            value = None
+        return value
 
-workbook_writer.save(os.path.join('/home/pmajka','output.xls'))
+    def _write_to_workbook(self, attribute, slice_index, value):
+        column, converter = COLUMN_MAPPING[attribute]
+        row = self._get_row_index(slice_index)
+
+        if attribute in ['image_size', 'padded_size']:
+            value = "x".join(map(str, value))
+
+        workbook_writer.get_sheet(0).write(row, column, str(value))
+
+    def _get_row_index(self, slice_index):
+        return _ROW_OF_THE_FIRST_SLICE + slice_index
+
+    def determine_and_set_file_size(self, new_slice):
+        filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
+        stat = os.stat(filename)
+        return stat.st_size
+
+    def determine_and_set_image_hash(self, new_slice):
+        filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
+        return md5sum(filename)
+
+    def determine_and_set_image_size(self, new_slice):
+        filename = os.path.join(SOURCE_IMAGES_DIR, new_slice.image_name)
+        return get_image_size_pixels(filename)
+
+    def determine_and_set_padding(self, new_slice=None):
+        max_w = max(map(lambda x: x.image_size[0], self.images.values()))
+        max_h = max(map(lambda x: x.image_size[1], self.images.values()))
+        pad_w = round_custom(max_w)
+        pad_h = round_custom(max_h)
+        return pad_w, pad_h
+
+if __name__ == "__main__":
+    pass
