206c1a473fe10f95bd6d70afb83f25d01cc9cbe3
Piotr Majka <pmajka@nencki.gov.pl>
2014-03-07 14:46:56 +0100

    ENCH: Removing files wich will not be included in the package.

---
+++ setenv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+#--------------------------------------------------------------------
+# Check if the necessary shell commands are available
+
+EXECUTABLES='mkdir rm cp tar ANTSJacobian ANTS WarpImageMultiTransform AverageAffineTransform AverageImages c3d c2d'
+
+for executable in ${EXECUTABLES}
+do
+    prog_path=`which $executable`
+    if [ $? -ne 0 ]; then
+        echo "(!) The ${executable} is not available."
+        echo "(!) The environment cannot be set. Exiting."
+        exit 1
+    fi
+done
+
+echo "(+) All necessary shell commands are available."
+
+#--------------------------------------------------------------------
+
+POS_DIR=`dirname $(readlink -f $0)`
+export PYTHONPATH=$PYTHONPATH:${POS_DIR}/possum/
+export PATH=$PATH:${POS_DIR}/bin/
