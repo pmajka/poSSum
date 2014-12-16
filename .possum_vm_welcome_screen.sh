91eda4211dcde350c34c579b5e7530723a320e56
Piotr Majka <pmajka@nencki.gov.pl>
2014-12-16 05:31:40 +0100

    ENCH: adding fancy script for the vm support.

---
+++ .possum_vm_welcome_screen.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -xe
+
+#TODO: Describe the purpose of this file
+
+#clear
+cat <<InputComesFromHERE
+                 _____ _____                 
+                / ____/ ____|                
+     _ __   ___| (___| (___  _   _ _ __ ___  
+    | '_ \ / _  \___  \___ \| | | | '_ \ _ \ 
+    | |_) | (_) |___) |___) | |_| | | | | | |
+    | .__/ \___/_____/_____/ \__,_|_| |_| |_|
+    | |                                      
+    |_|  Piotr Majka <p.majka@nencki.gov.pl>
+
+      Volumetric reconstruction framework
+      Framework version: ${possum_version}
+
+Type possum_test to conduct a basic sanity checks
+of the famework.
+
+You can find the most recent code snapshot on:
+https://github.com/pmajka/poSSum
+
+You can find the most recent version of the VM on:
+xxxx
+InputComesFromHERE
+
+# One should append the lines below to the .bashrc file
+#   function possum_test {
+
+#   echo ""
+#   echo ".------------------------------------------."
+#   echo "|         Conducting the tests             |"
+#   echo "*------------------------------------------*"
+#   echo ""
+
+#   cd ${possum_dir}
+#   python setup.py test
+#   cd 
+#   }
+
+#   possum_vm_filename=`find ~ -name .possum_vm_welcome_screen.sh`
+#   possum_dir=`dirname ${possum_vm_filename}`
+#   possum_version=`grep __version__ ${possum_dir}/possum/__init__.py | cut -f2 -d = | tr -d " '"`
+#   source ${possum_vm_filename}
