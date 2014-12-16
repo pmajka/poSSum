b14166b3b9fdb29fcaf62554c4e7d9aa13d562ac
Piotr Majka <pmajka@nencki.gov.pl>
2014-12-16 05:11:31 +0100

    ENCH: Adding the 2d section to 3d images example.

---
+++ test/test_stack_sections/stack_axial_example.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -xe
+
+# The serctions used in this example are downloaded from the
+# BrainMaps.org website
+# 
+# http://brainmaps.org/index.php?action=viewslides&datid=54
+#
+# Mikula, S., Trotts, I., Stone, J. M., & Jones, E. G. (2007).
+# Internet-enabled high-resolution brain mapping and virtual microscopy.
+# NeuroImage, 35(1), 9–15. doi:10.1016/j.neuroimage.2006.11.053
+# 
+# If you want to redownload the section, please uncomment the lines below 
+# 
+# wget -i list_of_axial_sections_to_download 
+# mogrify -r resize 25% *.jpg
+# zip exmaple_axial_cuts.zip *.jpg
+
+rm -rf *.jpg
+unzip exmaple_axial_cuts.zip
+pos_stack_reorient \
+    -i %04d.jpg \
+    -o stacked_axial_exmaple.nii.gz \
+    --stackingOptions 1020 1300 5 \
+    --permutation 0 1 2 \
+    --orientationCode RAS \
+    --setOrigin 0 0 0 \
+    --flipAxes 0 1 0 \
+    --setSpacing 1.0 1.0 3.5 \
+    --setType uchar \
+    --loglevel DEBUG 
+rm -rf *.jpg
+
---
+++ test/test_stack_sections/stack_coronal_example.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -xe
+
+# The serctions used in this example are downloaded from the
+# BrainMaps.org website
+# 
+# http://brainmaps.org/index.php?action=viewslides&datid=141
+# 
+
+# Mikula, S., Trotts, I., Stone, J. M., & Jones, E. G. (2007).
+# Internet-enabled high-resolution brain mapping and virtual microscopy.
+# NeuroImage, 35(1), 9–15. doi:10.1016/j.neuroimage.2006.11.053
+# 
+# If you want to redownload the section, please uncomment the lines below 
+# 
+# bash -xe download_coronal_sections.sh
+# 
+# i=0; 
+# for f in `ls -1 *.jpg`;
+# do 
+#     mv $f `printf %04d.jpg $i`;
+#     i=$[i+1];
+# done
+# 
+# zip example_coronal_sections.zip *.jpg
+# rm -rf *.jpg
+
+rm -rf *.jpg
+unzip example_coronal_sections.zip
+pos_stack_reorient \
+    -i %04d.jpg \
+    -o stacked_coronal_exmaple.nii.gz \
+    --stackingOptions 0 106 1 \
+    --permutation 0 2 1 \
+    --orientationCode RAS \
+    --setOrigin 0 0 0 \
+    --flipAxes 0 1 0 \
+    --setSpacing 0.15 0.3 0.15 \
+    --interpolation linear \
+    --setType uchar \
+    --loglevel DEBUG 
+
+pos_stack_reorient \
+    -i %04d.jpg \
+    -o stacked_coronal_exmaple_downsampled.nii.gz \
+    --stackingOptions 0 106 1 \
+    --permutation 0 2 1 \
+    --orientationCode RAS \
+    --setOrigin 0 0 0 \
+    --flipAxes 0 1 0 \
+    --resample 0.5 0.5 0.5 \
+    --setSpacing 0.3 0.6 0.3 \
+    --interpolation linear \
+    --setType uchar \
+    --loglevel DEBUG 
+rm -rf *.jpg
