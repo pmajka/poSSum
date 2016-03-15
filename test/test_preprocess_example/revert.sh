0619ec868af7241aef441bf98384630dbfff6712
Piotr Majka <pmajka@nencki.gov.pl>
2016-03-15 12:10:27 +1100

    ENCH: Improving tests in the test_preprocess_example test.

---
+++ test/test_preprocess_example/revert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is to be execyted from the Makefile only
+
+git --version;
+GIT_IS_AVAILABLE=${?};
+if [ ${GIT_IS_AVAILABLE} -eq 0 ];
+then
+    git checkout test_case.xls test_case_processed.pickle test_case_processed.xls header.sh
+fi;
+
+
